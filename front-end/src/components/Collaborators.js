46015570af2c7651e854d3d1f981f1927ec2075b
Jacob Thomas <jacobt3115@gmail.com>
2019-12-04 23:22:21 -0500

    added random names, and email modal

--- front-end/src/components/Collaborators.js
+++ front-end/src/components/Collaborators.js
@@ -1,24 +1,34 @@
 import React, { Component, Fragment } from "react"
 import Container from "react-bootstrap/Container"
-
+import Button from "react-bootstrap/Button"
+import { generateKeyPair } from "crypto";
 
 
 class Collaborators extends Component {
 
 	render() {
 		var collaborators = this.props.collaborators? Object.keys(this.props.collaborators).map((name, i)=>{
-			return <li>{name}:{this.props.collaborators[name]}</li>
+			return <Button style={buttonStyle} id= {name} onClick={this.props.showByName}>
+				{name}:{this.props.collaborators[name]}
+			</Button>
 		}):null;
 		return (
 			<Container>
 				<Fragment>
-					<ol>
+					<div class="btn-group-vertical">
 						{collaborators}
-					</ol>
+					</div>
 				</Fragment>
 			</Container>
 		)
 	}
+}
+
+const buttonStyle = {
+	backgroundColor: '#add8e6',
+	borderColor: "#838383",
+	borderRadius: 12,
+	color: "black"
 }
 
 
---
+++ front-end/src/components/EmailModal.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import Modal from "react-bootstrap/Modal"
+import  Button from "react-bootstrap/Button"
+import FormControl from "react-bootstrap/FormControl"
+import Container from "react-bootstrap/Container"
+
+class EmailModal extends Component {
+	constructor(props) {
+		super(props);	
+	}
+	render() {
+		return (
+		<Container>
+			<Modal
+			{...this.props}
+			size="sm"
+			aria-labelledby="contained-modal-title-vcenter"
+		  	>
+			<Modal.Header closeButton>
+			  <Modal.Title id="contained-modal-title-vcenter">
+				Send URL to Email
+			  </Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<FormControl
+					placeholder="Enter Email"
+					value={this.props.email}
+					onChange={this.props.handleEmailChange}
+
+				/>
+			</Modal.Body>
+			<Modal.Footer>
+			  <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+			  <Button variant="primary" onClick={this.props.handleSubmit}>Submit</Button>
+			</Modal.Footer>
+		  </Modal>
+		</Container>	
+		)
+	}
+}
+
+export default EmailModal
