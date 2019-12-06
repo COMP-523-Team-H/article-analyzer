e33872290f48325e4d453f293a166439f1853a2b
Jacob Thomas <jacobt3115@gmail.com>
2019-12-06 18:34:19 -0500

    fixed collab buttons scrollable div

--- front-end/src/components/Collaborators.js
+++ front-end/src/components/Collaborators.js
@@ -5,17 +5,18 @@ import { generateKeyPair } from "crypto";
 
 
 class Collaborators extends Component {
+	
 
 	render() {
 		var collaborators = this.props.collaborators? Object.keys(this.props.collaborators).map((name, i)=>{
-			return <Button style={buttonStyle} id= {name} onClick={this.props.showByName}>
+			return <Button className="buttons" style={buttonStyle} id= {name} onClick={this.props.showByName}>
 				{name}:{this.props.collaborators[name]}
 			</Button>
 		}):null;
 		return (
 			<Container>
 				<Fragment>
-					<div class="btn-group-vertical">
+					<div className="btn-group-vertical">
 						{collaborators}
 					</div>
 				</Fragment>
@@ -25,10 +26,12 @@ class Collaborators extends Component {
 }
 
 const buttonStyle = {
+	display: "inline-block",
 	backgroundColor: '#add8e6',
 	borderColor: "#838383",
 	borderRadius: 12,
-	color: "black"
+	color: "black",
+	height: "100px"
 }
 
 
