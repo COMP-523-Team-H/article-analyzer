d602d99c04eb9ad5d5abe1c8b10c9f8484a4a1fd
Yu-Ji19 <ji.yu.vy@gmail.com>
2019-12-08 22:53:09 -0500

    change

--- front-end/src/components/Workspace.js
+++ front-end/src/components/Workspace.js
@@ -255,6 +255,14 @@ class Workspace extends Component {
 
 	deleteAnnotation(annotation) {
 		rangy.remove(annotation.range, annotation.color);
+		this.state.annotations.forEach(annotation=>{
+			if(annotation.type === "text"){
+				rangy.highlight(annotation.range, annotation.color);
+			}else{
+				rangy.highlight_image(annotation.range, annotation.color);
+			}
+			
+		})
 		this.selectAnnotation(annotation);
 		req.post(hostname + '/api/annotation/delete', { id: annotation.id }).then((response) => {
 			var newCollaborators = {};
